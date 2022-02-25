28ee2923c3b3ae441e9d5374850c863b84c643f2
Damian Ciepiela <dciepiela@yahoo.com>
2022-02-25 21:28:12 +0100

    feat: chapter 12

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -16,4 +16,13 @@ class DollarTest {
         assertEquals("USD", Money.dollar(1).getCurrency());
     }
 
+    @Test
+    public void testSimpleAddition() {
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
+    }
+
 }
